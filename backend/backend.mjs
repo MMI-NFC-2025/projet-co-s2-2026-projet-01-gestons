4836ae78953353a0ac20b63834253ca32020f7cd
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2026-06-03 09:22:15 +0200

    bug régler

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,3 +1,9 @@
 import PocketBase from 'pocketbase';
+
 const pb = new PocketBase('https://gestons.girardin-tarby.fr');
+
+export function getImageUrl(record, filename) {
+    if (!filename) return null;
+    return pb.files.getURL(record, filename);
+}
 
